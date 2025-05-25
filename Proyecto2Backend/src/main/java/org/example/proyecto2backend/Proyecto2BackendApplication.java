3ccc31f5218ea9cd5b15229448bed00ad6ed85b3
Sebastian Alvarez Gomez <101519942+sebastian1233636@users.noreply.github.com>
2025-05-24 20:08:47 -0600

    Add files via upload

---
+++ Proyecto2Backend/src/main/java/org/example/proyecto2backend/Proyecto2BackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.proyecto2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto2BackendApplication.class, args);
+    }
+
+}
---
+++ Proyecto2Backend/src/test/java/org/example/proyecto2backend/Proyecto2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proyecto2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
